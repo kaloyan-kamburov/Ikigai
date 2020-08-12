baaacf68c1b3762b14b2b62e3bcdbdde886ee3ab
Kaloyan Kamburov <kaloyan.kamburov@experian.com>
2020-08-12 17:35:50 +0300

    style react select

--- src/pages/Wizard/goodAt.js
+++ src/pages/Wizard/goodAt.js
@@ -29,32 +29,42 @@ const GoodAt = () => {
       },
     });
   };
+  const styles = { option: (provided) => ({ ...provided, background: "red" }) };
 
   return (
     <>
-    <section className="section-dark">
-      <div className="shell">
+      <section className="section-dark">
+        <div className="shell">
+          <ul className="wizard">
+            <li className="wizard__step active">
+              <span className="rectangle active"></span>
+            </li>
+            <li className="wizard__step">
+              <span className="rectangle"></span>
+            </li>
+            <li className="wizard__step">
+              <span className="rectangle"></span>
+            </li>
+            <li className="wizard__step">
+              <span className="rectangle"></span>
+            </li>
+            <li className="wizard__step">
+              <span className="rectangle"></span>
+            </li>
+          </ul>
 
-        <ul className="wizard">
-          <li className="wizard__step active"><span className="rectangle active"></span></li>
-          <li className="wizard__step active"><span className="rectangle active"></span></li>
-          <li className="wizard__step"><span className="rectangle"></span></li>
-          <li className="wizard__step"><span className="rectangle"></span></li>
-          <li className="wizard__step"><span className="rectangle"></span></li>
-        </ul>
-        
           <div className="col-wrapper">
             <div className="col_60 form-bg">
               <div className="form-head">
                 <p className="formHead-title">Discover your purpose</p>
                 <ul className="page-number">
-                  <li>2</li> 
+                  <li>2</li>
                   <li>4</li>
                 </ul>
-              </div> 
-          
+              </div>
+
               <h1 className="form-title">What are you good at</h1>
-          
+
               <div className="form-select">
                 <Form
                   validate={(values) => {
@@ -77,35 +87,56 @@ const GoodAt = () => {
                           isMulti
                           value={props.initialValues.options}
                           options={options}
+                          styles={styles}
                         />
-                         <div className="form-btns">
-                            <button className="btn-small btn-light" disabled={props.invalid} type="submit">
-                              Continue
-                            </button>
+                        <div className="form-btns">
+                          <button
+                            className="btn-small btn-light"
+                            disabled={props.invalid}
+                            type="submit"
+                          >
+                            Continue
+                          </button>
                         </div>
                       </form>
                     );
                   }}
                 </Form>
               </div>
-         
-              <button onClick={() => history.push({ pathname: "/", state: {} })}>
+
+              <button
+                onClick={() => history.push({ pathname: "/", state: {} })}
+              >
                 Back to home
               </button>
-          
+
               <div className="form-foot">
                 <p>You have to add at least 3 items in order to continue.</p>
-                <p>Don’t worry about missing out - you can always change them later.</p>
+                <p>
+                  Don’t worry about missing out - you can always change them
+                  later.
+                </p>
               </div>
-      
             </div>
 
             <div className="col_40 form-questions">
-              <h2 className="form-questions-title">This question is meant to figure out your natural gifts: your talents and skills.</h2>
+              <h2 className="form-questions-title">
+                This question is meant to figure out your natural gifts: your
+                talents and skills.
+              </h2>
               <ul className="form-questions-list">
-                <li>  What parts of your current job are you effortless good at?</li>
-                <li>What are you among the best in your workplace, community (or even the whole world) at?</li>
-                <li>With some more education and experience, could you be among the best at what you do?</li>
+                <li>
+                  {" "}
+                  What parts of your current job are you effortless good at?
+                </li>
+                <li>
+                  What are you among the best in your workplace, community (or
+                  even the whole world) at?
+                </li>
+                <li>
+                  With some more education and experience, could you be among
+                  the best at what you do?
+                </li>
               </ul>
             </div>
           </div>
